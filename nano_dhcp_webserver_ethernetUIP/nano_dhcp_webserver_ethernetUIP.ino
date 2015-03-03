ca5e07ba3c43bec7dc204a7fb4f4b555a533b0c8
Letreguilly <letreguilly.sylvain@gmail.com>
2015-03-03 17:09:43 +0100

    add lib

--- nano_dhcp_webserver_ethernetUIP/nano_dhcp_webserver_ethernetUIP.ino
+++ nano_dhcp_webserver_ethernetUIP/nano_dhcp_webserver_ethernetUIP.ino
@@ -1,35 +1,49 @@
-/*
-  Web Server
-
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
-
- */
-
 #include <SPI.h>
-#include <Ethernet.h>
+#include <UIPEthernet.h>
+#include <EEPROM.h>
+#include <TrueRandom.h>
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
-
+char macstr[18];
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
 // (port 80 is default for HTTP):
 EthernetServer server(80);
 
 void setup() {
+  
+  
+  
+  if (EEPROM.read(1) == '#') {
+    for (int i = 3; i < 6; i++) {
+      mac[i] = EEPROM.read(i);
+    }
+  } else {
+    for (int i = 3; i < 6; i++) {
+      mac[i] = TrueRandom.randomByte();
+      EEPROM.write(i, mac[i]);
+    }
+    EEPROM.write(1, '#');
+  }
+  snprintf(macstr, 18, "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+  // Start up networking
+  Serial.print("DHCP (");
+  Serial.print(macstr);
+  Serial.print(")...");
+  Ethernet.begin(mac);
+  Serial.print("success: ");
+  Serial.println(Ethernet.localIP());
+  
+  
+  
+  
+  
+  
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -64,12 +78,12 @@ void loop() {
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
+        //char c = client.read();
+        //Serial.write(c);
         // if you've gotten to the end of the line (received a newline
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
+        //if (c == '\n' && currentLineIsBlank) {
           // send a standard http response header
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
@@ -78,26 +92,18 @@ void loop() {
           client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
+          client.println("hello world !");
           client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
+          //break;
+        //}
+        //if (c == '\n') {
           // you're starting a new line
-          currentLineIsBlank = true;
-        }
-        else if (c != '\r') {
+          //currentLineIsBlank = true;
+        //}
+        //else if (c != '\r') {
           // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
+          //currentLineIsBlank = false;
+        //}
       }
     }
     // give the web browser time to receive the data
